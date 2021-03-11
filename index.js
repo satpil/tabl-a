4ab70e52b6d3b1f3ea506ece37fe9eeebaa50fde
pillai <satish.pillai888@gmail.com>
2021-03-11 09:09:01 +0530

    server

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const express = require('express');
 const app = express();
 const data = require('./routes/data');
 
-const config = require('./config');
-// const config = require('./config');
-// const HOST = config.HOST;
-// const PASSWORD = config.PASSWORD;
-// const DATABASE = config.DATABASE;
-// const MONGODB_URI = process.env.MONGODB_URI; 
 const MONGODB_URI = 'mongodb+srv://thunderpi:Test@123@thunderpi.rmgof.mongodb.net/thunderpi?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URI,{
   auto_reconnect: true,
@@ -27,5 +21,5 @@ app.use(express.urlencoded({
 app.use('/data',data)
 
 /** local server running on port 3000 */
-app.listen(3000, () => console.log('listening to port 3000'))
+app.listen(process.env.PORT || 5000, () => console.log('listening to port 3000'))
 module.exports = app;
